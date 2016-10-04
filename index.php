3cc4627b1932069ce78be1d8df27b11a8999267e
RedstoneGuy <redstone23@outlook.com>
2016-10-03 21:34:35 -0700

    Start to a new website!!

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<Head>
+		<title>
+			Robot Sketches
+		</title>
+	</head>
+	<body>
+		Welcome to my website!! ;3<br/>
+		<img src="images/robotsketch.jpg"
+		 alt="its a sketch of a robot!"/>
+	</body>
+</html>
