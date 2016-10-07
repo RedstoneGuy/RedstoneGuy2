bd19fb6b918cf236a21b3deb79989f5f23da0708
RedstoneGuy <redstone23@outlook.com>
2016-10-06 22:17:51 -0700

    More changes
    yay!!

--- index.php
+++ index.php
@@ -3,10 +3,45 @@
 		<title>
 			Robot Sketches
 		</title>
+		<style>
+			body {
+				background-image: url("images/simplerobot.jpg");
+			}
+			.robotimages {
+				width:300px;
+			}
+			.robotimages:hover {
+				width:400px;
+			}
+		</style>
 	</head>
 	<body>
-		Welcome to my website!! ;3<br/>
+		Welcome to my website!! Of fan art sketches and more!<br/>
+		<a href="http://www.google.com?q=robots">search robots on google</a> <br/>
+		<a href="https://en.wikipedia.org/wiki/Robot">robots wikipedia</a> <br/>
 		<img src="images/robotsketch.jpg"
-		 alt="its a sketch of a robot!"/>
+		 alt="its a sketch of a robot!" class="robotimages"/>
+		 <img src="images/robotsketch2.jpg"
+		alt="its a sketch of a cute easy robot!" class="robotimages"/>
+		<img src="images/robotsketch3.jpg"
+		alt="its a sketch of a realistic robot!" class="robotimages"/>
+		<img src="images/wheatly.jpg"
+		alt="wheatly from portal 2 aka a robot core!" class="robotimages"/>
+		<img src="images/dog.jpg"
+		alt="its a picture of dog from half life 2!" class="robotimages"/>
+		<img src="images/hunter.jpg"
+		alt="a hunter from half life 2 episode 2!" class="robotimages"/>
+		<img src="images/machinarium.jpg"
+		alt="a game called machinarium main screen" class="robotimages"/>
+		<img src="images/robotconcept.jpg"
+		alt="a fighting robot concept!" class="robotimages"/>
+		<img src="images/simplerobot.jpg"
+		alt="a simple cute robot with glowing body parts!" class="robotimages"/>
+		<img src="images/robotsquid.jpg"
+		alt="a picture of a robot squid in space!" class="robotimages"/>
+		<img src="images/robotrat.jpg"
+		alt="a robot rat with glowing blue parts!" class="robotimages"/>
+		
 	</body>
 </html>
+		
